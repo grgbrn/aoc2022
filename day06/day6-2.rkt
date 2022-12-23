00d143118c0c3fa8cb98bb37a4eec2f89738c95c
Greg Brown <grgbrn@gmail.com>
2022-12-23 18:45:59 +0100

    day6, again

--- day06/day6-2.rkt
+++ day06/day6-2.rkt
@@ -3,18 +3,52 @@
 (module+ test
   (require rackunit))
 
-(define (unique-char-count s)
-  (set-count (list->set (string->list s))))
+;;
+;; second impl of day6, should be faster
+;; based on https://f4t.dev/software/go-performance-memory/
+;;
 
+;; probably more idiomatic use of "for"
+
+;; check if a string contains duplicate chars
+;; using a set
+(define (has-duplicates-1? input)
+    (for/fold ([seen (set)]
+               #:result (not (= (set-count seen) (string-length input))))
+              ([elt (in-string input)]
+               #:break (set-member? seen elt))
+      (set-add seen elt)))
+
+;; variation using a byte array
+(define (has-duplicates-2? s)
+  ;; set an individual bit in a byte array
+  (define (set-bit bs b)
+    (let-values ([(i tmp) (quotient/remainder b 8)])
+      (let* ([mask (arithmetic-shift 1 tmp)]
+             [masked (bitwise-xor (bytes-ref bs i) mask)]
+             [retval (bytes-copy bs)])
+        (bytes-set! retval i masked)
+        retval)))
+  ;; check if a bit is set in a byte array
+  (define (bit-set? bs b)
+    (let-values ([(i tmp) (quotient/remainder b 8)])
+      (bitwise-bit-set? (bytes-ref bs i) tmp)))
+
+  (for/fold ([seen (make-bytes 32 0)]
+             [checked 0]
+             #:result (not (= checked (string-length s))))
+            ([char (in-string s)]
+             #:break (bit-set? seen (char->integer char)))
+    (values
+     (set-bit seen (char->integer char))
+     (add1 checked))))
+
+;; returns the index of the first character following a unique sequence
+;; of characters in "input"
 (define (index-after-prefix prefix-length input)
-  (let* ([offsets (filter (lambda (pair) (= (cdr pair) prefix-length))
-                          (map ;; generate a list of pairs (offset . unique-count)
-                           (lambda (n) (cons
-                                        n
-                                        (unique-char-count (substring input n (+ n prefix-length)))))
-                           (range (- (string-length input) prefix-length))))]
-         [match (first offsets)])
-    (+ (car match) prefix-length)))
+  (for/last ([i (range 0 (add1 (- (string-length input) prefix-length)))]
+             #:final (not (has-duplicates-2? (substring input i (+ i prefix-length)))))
+    (+ i prefix-length)))
 
 (define (start-of-packet input)
   (index-after-prefix 4 input))
@@ -47,4 +81,3 @@
 (time
  (displayln "start of message (2):")
  (start-of-message input1))
-
